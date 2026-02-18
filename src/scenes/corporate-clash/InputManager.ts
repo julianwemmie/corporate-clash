773f06594c4682d3bc6fcdbdbf8e61b84748c228
sendtwothreeyears <franksheikh@icloud.com>
2026-02-18 11:54:57 -0500

    [add] resolve merge conflict

--- src/scenes/corporate-clash/InputManager.ts
+++ src/scenes/corporate-clash/InputManager.ts
@@ -1,6 +1,26 @@
 import type { Manager, CorporateWorld, GridPos } from './types.js';
 
 export class InputManager implements Manager {
+  onKeyDown(world: CorporateWorld, key: string): void {
+    if (key === 'Space') {
+      // //
+      // else if (world.state === "gameOver") {
+      //   resetWorld(world);
+      //   world.state = "start";
+      // }
+      // return;
+    }
+
+    // if (world.state !== "playing") return;
+
+    // const dir = KEY_DIRECTION[key];
+    // if (!dir) return;
+
+    // if (dir === OPPOSITE[world.player.direction]) return;
+
+    // world.player.nextDirection = dir;
+  }
+
   onRightClick(world: CorporateWorld, gridPos: GridPos): void {
     console.log('Right click at', gridPos);
   }
